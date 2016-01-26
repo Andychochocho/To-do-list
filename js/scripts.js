85355b9a454256347e5f70a2d1f99fc054d5e643
andrew cho and pablo vera <cho-andrew@hotmail.com>
2016-01-25 16:49:37 -0800

    finalize project with jquery functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
+// Business Logic
 function Task (trash) {
   this.trash = trash;
 }
+
+// User-interface
+$(document).ready(function(){
+  $("form#new-task").submit(function(){
+    event.preventDefault();
+    var inputtedTask = $('input#new-taskNeedComplete').val();
+    var newTask = new Task(inputtedTask);
+
+    $('ul#listTasks').append("<li><span class='taskNames'>" + inputtedTask +"</li>");
+
+    $("li .taskNames").click(function(){
+      var transferTasks = $("li .taskNames").detach();
+      $("#listCompletedTasks").append(transferTasks);
+    });
+
+    inputtedTask = $('input#new-taskNeedComplete').val("");
+
+    $(".taskNames").last().click(function(){
+    $("#hide-results").show();
+    $(".task-text").text(newTask.task);
+
+  });
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
 describe ("Task",function(){
   it("will add tasks to be completed", function() {
     var testTask = new Task ("Taking the trash out")
-    expect(testTask.trash).to.equal("Taking the trash out");
+    expect(testTask.taskNeedComplete).to.equal("Taking the trash out");
   });
 });
